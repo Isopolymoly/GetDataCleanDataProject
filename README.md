aa28a00d0abb170e9b1fbb4d7bdcfc9fe647c2da
Isopolymoly <molybdenite@earthlink.net>
2015-02-18 13:25:18 -0600

    added documentation
    cleaned up variable names (removed repeated .'s)
    added variable descriptions to codebook.Rmd
    generated codebook.html with knitr

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GetDataCleanDataProject
 Coursera Data Science course: Get Data, Clean Data class project
+
+
+
+
+# to read table back into R:
+check_table <- read.table("tidy_summary.txt", header=TRUE)
--- run_analysis.R
+++ run_analysis.R
@@ -2,21 +2,33 @@
 # Course Project
 # February 2015
 
+## preliminary step:  set working directory to parent directory for dataset's 
+##  example:  working directory contains directory "UCI HAR Dataset/" 
 
-setwd("c:/users/heather/desktop/datascience/getdatacleandata/course_project")
+setwd("c:/users/heather/desktop/datascience/getdatacleandata/course_project/")
 
+
+##  Checklist quoted from project instructions:
 # You should create one R script called run_analysis.R that does the following. 
 # 
 # done 1) Merges the training and the test sets to create one data set.
 # done 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
-# done  3) Uses descriptive activity names to name the activities in the data set
-# 4) Appropriately labels the data set with descriptive variable names. 
-# 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# done 3) Uses descriptive activity names to name the activities in the data set
+# done 4) Appropriately labels the data set with descriptive variable names. 
+# done 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 # http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
 
+### References
+##  class lectures and discussion forum
+##  Stack Overflow
+#      http://stackoverflow.com/questions/28549045/dplyr-select-error-found-duplicated-column-name/28576595#28576595
 
+
+################################################################################
+################################################################################
+################################################################################
 
 
 # load packages
@@ -25,9 +37,7 @@ library("dplyr")
 library("tidyr")
 
 
-
-
-###  Load data
+###  load data
 
 # training set
 filename_training_x <- "UCI HAR Dataset/train/X_train.txt"
@@ -72,9 +82,8 @@ training_dataset$group <- "train"
 
 #######################
 testing_x <- read.table(filename_testing_x)
+
 ## add descriptive column names from feature list
-
-
 names(testing_x) <- col_names
 
 
@@ -83,14 +92,14 @@ names(testing_subjects) <- "subjectID"
 
 
 # read activity log, rename column
-# later: read labels file; apply labels as col names to log
+# later in script: read labels file; replace integers with string activity descriptions
 testing_activities <- read.table(filename_testing_activities)
 names(testing_activities) <- "activities"
 
 
+# group test data into a single dataset and create a new column to with group (test or train) name
 testing_dataset <- cbind(testing_subjects, testing_activities, testing_x)
 testing_dataset$group <- "test"
-
 
 
 
@@ -113,7 +122,7 @@ if (length(intersect(unique(training_subjects$V1), unique(testing_subjects$V1)))
 
 
 # remove data objects for original (separate) files
-#rm("training_x",   "training_subjects", "training_activities", "testing_x", "testing_activities", "testing_subjects")
+rm("training_x",   "training_subjects", "training_activities", "testing_x", "testing_activities", "testing_subjects")
 
 
 
@@ -134,34 +143,36 @@ names(activity_labels) <- c("activity_number", "activity_name")
 #http://stackoverflow.com/questions/10002536/how-do-i-replace-numeric-codes-with-value-labels-from-a-lookup-table
 #df$profession.str <- names(profession.code)[match(df$profession, profession.code)]
 
-
-# FIXME  # TODO  NA's introduced in lookup table operation here 
 activities_by_name <- activity_labels$activity_name[match(full_dataset$activities, activity_labels$activity_number)]
 full_dataset$activities <- activities_by_name
 
-#activity_labels$activity_name[match(training_dataset$activities, activity_labels$activity_number)]
 
 
 
 # make *valid* names for columns (original names have invalid special characters)
-mn0 <- make.names(names=names(full_dataset), unique=TRUE, allow_ = TRUE)
- names(full_dataset) <- mn0
+valid_names <- make.names(names=names(full_dataset), unique=TRUE, allow_ = TRUE)
+
+# remove extra periods introduced by make.names
+##   replace repeated periods or periods at the end of a variable name with "" 
+cleaned_names <- sub("\\.(\\.)+|\\.$", "", valid_names)
+
+names(full_dataset) <- cleaned_names
 
 ################################################################
 
 ### Extract measurements on the mean and std
 full_tbl <- tbl_df(full_dataset)
 rm("full_dataset")
-# selected <- select(full_tbl, contains("mean")) # errors about duplicated column names, doesn't recognize numeric string as unique at end of col names
 
-#http://stackoverflow.com/questions/28549045/dplyr-select-error-found-duplicated-column-name/28576595#28576595
+
 
 
 selected_tbl <- select(full_tbl, subjectID, activities, contains("mean"), contains("std"))
 
 ### Descriptive variable names
 
-# TODO
+# subject, activity, and feature names were created in previous steps 
+
 
 
 
@@ -175,11 +186,18 @@ tidy_summary <- selected_tbl %>%
   summarise_each(funs(mean))%>% 
   print
 
-
+View(tidy_summary)
 ## 
+
+
 ### write tidy data set to .txt file
+#  this txt file is uploaded on the course project assignment page
 write.table(tidy_summary, file="tidy_summary.txt", col.names=TRUE, row.name=FALSE) 
 
 
-# to read table back into R:
+# to read new tidy dataset table back into R:
 check_table <- read.table("tidy_summary.txt", header=TRUE)
+
+
+##  Generate variable list for codebook: 
+write.table(names(tidy_summary), file="forcodebook.txt")
